93a8b1c73463370f650c694f4bc79a7151e10455
Cally Hou <104803387+callytat@users.noreply.github.com>
2022-10-17 14:07:17 -0700

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/Hello.java
@@ -0,0 +1,15 @@
+/**
+ *@author Cally
+ *email: callytat001@gmail.com
+ *date: 10/17/22
+ *purpose: My first Hello World program in an IDE
+ */
+
+// Use YOUR Group Id value here if it is different from com.mthree
+package com.cally.helloworld; 
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Cally!");
+    }
+}
